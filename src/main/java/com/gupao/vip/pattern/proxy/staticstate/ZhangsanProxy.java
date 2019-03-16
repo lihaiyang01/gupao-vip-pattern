14355cfb91cc5453e439d0aa73cf41f319e3e39c
oceanli <oceanli@aijiatui.com>
2019-03-16 11:28:20 +0800

    添加注释

--- src/main/java/com/gupao/vip/pattern/proxy/staticstate/ZhangsanProxy.java
+++ src/main/java/com/gupao/vip/pattern/proxy/staticstate/ZhangsanProxy.java
@@ -6,6 +6,11 @@ import com.gupao.vip.pattern.proxy.bean.Person;
  * @author: oceanLi
  * @date: 2019/3/16 0016 10:52
  * @description:
+ *
+ *
+ * 静态代理总结：
+ * 优点：可以做到在符合开闭原则的情况下对目标对象进行功能扩展。
+ * 缺点：代理xu*
  */
 public class ZhangsanProxy implements Person {
 
